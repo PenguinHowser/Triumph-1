9d9461da864480d2504774873078248fea92ff37
Chicorak <35471036+Chicorak@users.noreply.github.com>
2024-02-09 22:43:39 -0600

    trains, plains, and muranges
    cleaned up character, camera and stuff just need to fix cam shoulder issues and get started on cutscenes

---
+++ Entities/player/cutscene_manager.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+@export var frame = 0
+
+func _process(delta):
+	pass
--- Entities/player/player.gd
+++ Entities/player/player.gd
@@ -1,70 +1,74 @@
 extends CharacterBody3D
 
+#movement_animator
+@onready var movement = $Movement
+@onready var cam_movement = $CamMovement
 
-const SPEED = 50.0
-const JUMP_VELOCITY = 4.5
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-@onready var pcam = $PhantomCamera3D
 @onready var sprite = $MeshInstance3D
+@onready var cam_origin = $CamOrigin
+
+var max_speed = 25.0
+var speed = max_speed
+
+var jump_velocity = 4.5
+
+var follow_cam = false
 
 var mouse_sensitivity: float = 0.05
 
 var min_yaw: float = 0
 var max_yaw: float = 360
-
 var min_pitch: float = -90
 var max_pitch: float = 90
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 
 func _unhandled_input(event) -> void:
-  # Trigger whenever the mouse moves.
-	if event is InputEventMouseMotion:
-		
-		var pcam_rotation_degrees: Vector3
+	if event is InputEventMouseMotion :
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))
 
-		# Assigns the current 3D rotation of the SpringArm3D node - to start off where it is in the editor.
-		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
-
-		# Change the X rotation.
-		pcam_rotation_degrees.x -= event.relative.y * mouse_sensitivity
-			
-		# Clamp the rotation in the X axis so it can go over or under the target.
-		pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, min_pitch, max_pitch)
-		# Change the Y rotation value.
-		pcam_rotation_degrees.y -= event.relative.x * mouse_sensitivity
-			
-		# Sets the rotation to fully loop around its target, but without going below or exceeding 0 and 360 degrees respectively.
-		pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, min_yaw, max_yaw)
-			
-		# Change the SpringArm3D node's rotation and rotate around its target.
-		pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
+		cam_origin.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+		cam_origin.rotation.x = clamp(cam_origin.rotation.x, deg_to_rad(-45), deg_to_rad(45))
 
 func _physics_process(delta):
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
+		
+	if Input.is_action_pressed("aim"):
+		cam_movement.play("shoulder")
+	elif Input.is_action_just_released("aim"):
+		cam_movement.play("RESET")
 	# Add the gravity.
 	if not is_on_floor():
+		speed = max_speed / 1.5
 		velocity.y -= gravity * delta
-
+	else:
+		speed = max_speed
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "backward")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		sprite.global_rotation.y = pcam.global_rotation.y
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		#sprite.global_rotation.y 
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		movement.play("walk")
+		follow_cam = true
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		movement.play("idle")
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
+		follow_cam = false
 
 	move_and_slide()
